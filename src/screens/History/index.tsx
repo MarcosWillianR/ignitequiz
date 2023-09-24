ee892bc1f7783e3627c7bc2ea1b83c4b8845eb98
Marcos Willian <markusuuuu@gmail.com>
2023-09-24 04:15:41 -0300

    studying: layout animations

--- src/screens/History/index.tsx
+++ src/screens/History/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { View, ScrollView, TouchableOpacity, Alert } from 'react-native';
 import { HouseLine } from 'phosphor-react-native';
+import Animated, { Layout, SlideInRight, SlideOutRight } from 'react-native-reanimated';
 
 import { Header } from '../../components/Header';
 import { HistoryCard, HistoryProps } from '../../components/HistoryCard';
@@ -65,12 +66,16 @@ export function History() {
       >
         {
           history.map((item) => (
-            <TouchableOpacity
-              key={item.id}
-              onPress={() => handleRemove(item.id)}
+            <Animated.View 
+              key={item.id} 
+              layout={Layout.springify()}
+              entering={SlideInRight}
+              exiting={SlideOutRight}
             >
-              <HistoryCard data={item} />
-            </TouchableOpacity>
+              <TouchableOpacity onPress={() => handleRemove(item.id)}>
+                <HistoryCard data={item} />
+              </TouchableOpacity>
+            </Animated.View>
           ))
         }
       </ScrollView>
