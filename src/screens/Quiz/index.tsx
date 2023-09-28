1f8d1282c540929f4d6c03111f77920e3a30bce1
Marcos Willian <markusuuuu@gmail.com>
2023-09-27 22:43:38 -0300

    studying (WIP): create header for scroll animation

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Alert, View } from 'react-native';
+import { Alert, View, Text } from 'react-native';
 import Animated, { 
   useAnimatedStyle, 
   useSharedValue, 
@@ -14,6 +14,8 @@ import { useNavigation, useRoute } from '@react-navigation/native';
 import { styles } from './styles';
 
 import { QUIZ } from '../../data/quiz';
+import { THEME } from '../../styles/theme';
+
 import { historyAdd } from '../../storage/quizHistoryStorage';
 
 import { Loading } from '../../components/Loading';
@@ -21,6 +23,7 @@ import { Question } from '../../components/Question';
 import { QuizHeader } from '../../components/QuizHeader';
 import { ConfirmButton } from '../../components/ConfirmButton';
 import { OutlineButton } from '../../components/OutlineButton';
+import { ProgressBar } from '../../components/ProgressBar';
 
 interface Params {
   id: string;
@@ -128,6 +131,16 @@ export function Quiz() {
     }
   });
 
+  const fixedProgressBarStyles = useAnimatedStyle(() => {
+    return {
+      position: 'absolute',
+      paddingTop: 50,
+      backgroundColor: THEME.COLORS.GREY_500,
+      width: '110%',
+      left: '-5%',
+    }
+  })
+
   useEffect(() => {
     const quizSelected = QUIZ.filter(item => item.id === id)[0];
     setQuiz(quizSelected);
@@ -146,6 +159,17 @@ export function Quiz() {
 
   return (
     <View style={styles.container}>
+      <Animated.View style={fixedProgressBarStyles}>
+        <Text style={styles.title}>
+          {quiz.title}
+        </Text>
+
+        <ProgressBar 
+          total={quiz.questions.length} 
+          current={currentQuestion + 1} 
+        />
+      </Animated.View>
+
       <Animated.ScrollView
         showsVerticalScrollIndicator={false}
         contentContainerStyle={styles.question}
